25d453f621164f722aad689e403a1aa7cd0a519a
Jacob Pedersen <jacob@jacobpedersen.dk>
2017-10-03 12:10:09 +0200

    Updated testRabbitMQ with initial test example project

--- testRabbitMQ/Consumer/src/main/kotlin/dk/jacobpedersen/testRabbitMQ/Consumer/Main.kt
+++ testRabbitMQ/Consumer/src/main/kotlin/dk/jacobpedersen/testRabbitMQ/Consumer/Main.kt
@@ -3,7 +3,34 @@
  */
 package dk.jacobpedersen.testRabbitMQ.Consumer
 
+import java.io.IOException
+import com.rabbitmq.client.*;
+import kotlinx.coroutines.experimental.delay
+
+
+private val QUEUE_NAME = "hello"
+
 fun main(args : Array<String>) {
     println("Hello, world!")
     println("Start")
+    val factory = ConnectionFactory()
+    factory.host = "172.17.0.2"
+    factory.port= 5672
+    factory.username = "guest"
+    factory.password = "guest"
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+    println(" [*] Waiting for messages. To exit press CTRL+C")
+
+    val consumer = object : DefaultConsumer(channel) {
+        @Throws(IOException::class)
+        override fun handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: ByteArray) {
+            val message = String(body, charset("UTF-8"))
+            println(" [x] Received '$message'")
+//            Thread.sleep(500)
+        }
+    }
+    channel.basicConsume(QUEUE_NAME, true, consumer)
 }
--- testRabbitMQ/Producer/src/main/kotlin/dk/jacobpedersen/testRabbitMQ/Producer/Main.kt
+++ testRabbitMQ/Producer/src/main/kotlin/dk/jacobpedersen/testRabbitMQ/Producer/Main.kt
@@ -2,8 +2,36 @@
  * Created by jacob on 2017-09-25 (YYYY-MM-DD).
  */
 package dk.jacobpedersen.testRabbitMQ.Producer
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.Channel
+
+private val QUEUE_NAME = "hello"
+
 
 fun main(args : Array<String>) {
     println("Hello, world!")
     println("Start")
+    val factory = ConnectionFactory()
+    factory.host = "172.17.0.2"
+    factory.port= 5672
+    factory.username = "guest"
+    factory.password = "guest"
+    val connection = factory.newConnection()
+    val channel = connection.createChannel()
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+    val message = "Hello World!"
+    for (i in 1..1000) {
+        channel.basicPublish("", QUEUE_NAME, null, message.toByteArray(charset("UTF-8")))
+        println(" [x] Sent '$message'")
+    }
+    channel.close()
+    connection.close()
+}
+
+class send() {
+    val QueueName : String = "hello"
+
+
 }
